bcef3fad532eeb4d07eaec74b412e425958b515a
Hubert <kiszkahub@gmail.com>
2025-07-02 20:09:56 +0200

    some changes

--- src/main/java/com/kiszka/prj/controllers/KidController.java
+++ src/main/java/com/kiszka/prj/controllers/KidController.java
@@ -2,7 +2,6 @@ package com.kiszka.prj.controllers;
 
 import com.kiszka.prj.entities.Kid;
 import com.kiszka.prj.services.KidService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,12 +11,10 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/kids")
 public class KidController {
-
     private final KidService kidService;
     public KidController(KidService kidService) {
         this.kidService = kidService;
     }
-
     @PostMapping("/new")
     public ResponseEntity<Kid> addKid(@RequestBody Kid kid) {
         try {
@@ -27,28 +24,25 @@ public class KidController {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteKid(@PathVariable int id) {
         try {
             if (kidService.getKidById(id).isPresent()) {
                 kidService.deleteKid(id);
-                return new ResponseEntity<>("Dziecko zostało usunięte", HttpStatus.OK);
+                return new ResponseEntity<>("Kid deleted", HttpStatus.OK);
             } else {
-                return new ResponseEntity<>("Dziecko o podanym ID nie istnieje", HttpStatus.NOT_FOUND);
+                return new ResponseEntity<>("Kid of such ID does not exist", HttpStatus.NOT_FOUND);
             }
         } catch (Exception e) {
-            return new ResponseEntity<>("Błąd podczas usuwania dziecka", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>("Error deleting kid", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-
     @GetMapping("/{id}")
     public ResponseEntity<Kid> getKidById(@PathVariable int id) {
         return kidService.getKidById(id)
                 .map(kid -> new ResponseEntity<>(kid, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
-
     @GetMapping
     public ResponseEntity<List<Kid>> getAllKids() {
         try {
--- src/main/java/com/kiszka/prj/repositories/KidRepository.java
+++ src/main/java/com/kiszka/prj/repositories/KidRepository.java
@@ -3,6 +3,4 @@ package com.kiszka.prj.repositories;
 import com.kiszka.prj.entities.Kid;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface KidRepository extends JpaRepository<Kid, Integer> {}
