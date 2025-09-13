2addbcd4bc4e0aea355ae3eaa98d312c3f310414
kiszkahubert <kiszkahub@gmail.com>
2025-09-13 20:36:13 +0200

    few tweaks

--- native/app/src/main/java/com/kiszka/kiddify/MainPageActivity.java
+++ native/app/src/main/java/com/kiszka/kiddify/MainPageActivity.java
@@ -1,14 +1,16 @@
 package com.kiszka.kiddify;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
+import com.kiszka.kiddify.adapters.TaskAdapter;
+import com.kiszka.kiddify.database.DataManager;
 import com.kiszka.kiddify.databinding.ActivityMainPageBinding;
+
+import java.util.ArrayList;
 
 public class MainPageActivity extends AppCompatActivity {
     private ActivityMainPageBinding binding;
@@ -18,5 +20,20 @@ public class MainPageActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         this.binding = ActivityMainPageBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        String kidName = DataManager.getInstance(this).getKidName();
+        binding.welcomeText.setText("Cześć, "+kidName+ "!");
+        TaskAdapter taskAdapter = new TaskAdapter(this, new ArrayList<>());
+        binding.tasksRecyclerView.setAdapter(taskAdapter);
+        DataManager.getInstance(this).getAllTasks().observe(this, taskList -> {
+            if (taskList != null && !taskList.isEmpty()) {
+                taskAdapter.setTasks(taskList);
+                binding.emptyTasksText.setVisibility(View.GONE);
+                binding.tasksRecyclerView.setVisibility(View.VISIBLE);
+            } else {
+                binding.emptyTasksText.setVisibility(View.VISIBLE);
+                binding.tasksRecyclerView.setVisibility(View.GONE);
+            }
+        });
+
     }
 }
--- native/app/src/main/java/com/kiszka/kiddify/adapters/TaskAdapter.java
+++ native/app/src/main/java/com/kiszka/kiddify/adapters/TaskAdapter.java
@@ -8,38 +8,47 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.kiszka.kiddify.databinding.ItemTaskBinding;
 import com.kiszka.kiddify.models.Task;
+import com.kiszka.kiddify.models.TaskData;
 
 import java.util.List;
 
 public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder> {
-    private List<Task> tasks;
+    private List<TaskData> tasks;
     private LayoutInflater inflater;
 
-    public TaskAdapter(Context context, List<Task> tasks){
+    public TaskAdapter(Context context, List<TaskData> tasks) {
         this.inflater = LayoutInflater.from(context);
         this.tasks = tasks;
     }
     @NonNull
     @Override
     public TaskViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        ItemTaskBinding binding = ItemTaskBinding.inflate(inflater, parent, false);
+        return new TaskViewHolder(binding);
     }
-
     @Override
     public void onBindViewHolder(@NonNull TaskViewHolder holder, int position) {
-
+        TaskData task = tasks.get(position);
+        holder.binding.taskTitle.setText(task.getTitle());
+        holder.binding.taskTime.setText(task.getTaskStart() + " - " + task.getTaskEnd());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return tasks.size();
+    }
+    public void setTasks(List<TaskData> tasks) {
+        this.tasks = tasks;
+        notifyDataSetChanged();
     }
 
-    static class TaskViewHolder extends RecyclerView.ViewHolder{
-
-        public TaskViewHolder(@NonNull View itemView) {
-            super(itemView);
+    public static class TaskViewHolder extends RecyclerView.ViewHolder {
+        public ItemTaskBinding binding;
+        public TaskViewHolder(ItemTaskBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
         }
     }
 }
