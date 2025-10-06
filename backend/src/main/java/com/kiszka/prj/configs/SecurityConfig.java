81d0d84b5a6a2c74e1f7c9e42c618e8f5c19f87a
kiszkahubert <kiszkahub@gmail.com>
2025-10-06 17:34:12 +0200

    .

--- backend/src/main/java/com/kiszka/prj/configs/SecurityConfig.java
+++ backend/src/main/java/com/kiszka/prj/configs/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/chat-websocket/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
